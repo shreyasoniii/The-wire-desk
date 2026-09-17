abf6e7c05436f51db60814685630800562a8c870
Shreya Soni <shreyasonii181@gmail.com>
2026-09-17 14:38:29 +0530

    Remove node_modules and update gitignore

--- backend/node_modules/@mistralai/mistralai/esm/models/components/deletepromptresponse.d.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod/v4";
-import { Result as SafeParseResult } from "../../types/fp.js";
-import { SDKValidationError } from "../errors/sdkvalidationerror.js";
-export type DeletePromptResponse = {};
-/** @internal */
-export declare const DeletePromptResponse$inboundSchema: z.ZodType<DeletePromptResponse, unknown>;
-export declare function deletePromptResponseFromJSON(jsonString: string): SafeParseResult<DeletePromptResponse, SDKValidationError>;
-//# sourceMappingURL=deletepromptresponse.d.ts.map
